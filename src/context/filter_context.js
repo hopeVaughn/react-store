1be0069c936273eccae0a95d800b9573f3c0d634
hopeVaughn <hopevaughnwarren@gmail.com>
2022-10-23 13:54:52 -0700

    add color filter

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,7 +17,18 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: 'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
 }
+
 
 const FilterContext = React.createContext()
 
@@ -30,8 +41,9 @@ export const FilterProvider = ({ children }) => {
   }, [products])
 
   useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS })
     dispatch({ type: SORT_PRODUCTS })
-  }, [products, state.sort])
+  }, [products, state.sort, state.filters])
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW })
@@ -41,9 +53,25 @@ export const FilterProvider = ({ children }) => {
   }
 
   const updateSort = (e) => {
-    const name = e.target.name;
+    // const name = e.target.name;
     const value = e.target.value;
     dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
+  const updateFilters = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+    if (name === 'category') {
+      value = e.target.textContent
+    }
+    if (name === 'color') {
+      value = e.target.dataset.color
+    }
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
+  }
+
+  const clearFilters = () => {
+    //
   }
 
   const value = {
@@ -51,6 +79,8 @@ export const FilterProvider = ({ children }) => {
     setGridView,
     setListView,
     updateSort,
+    updateFilters,
+    clearFilters
   }
 
   return (
