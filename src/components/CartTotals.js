3b8eb38aa990b32af39710cd1b22fe39bee3912d
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-07 09:10:33 -0800

    add conditional rendering to cartTotals,sidebar

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -6,7 +6,8 @@ import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
 
 const CartTotals = () => {
-  const { total_amount, shipping_fee } = useCartContext()
+  const { total_amount, shipping_fee } = useCartContext();
+  const { myUser, loginWithRedirect } = useUserContext();
   return (
     <Wrapper>
       <div className="">
@@ -18,9 +19,9 @@ const CartTotals = () => {
             order total : <span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        <Link to="/checkout" className='btn'>
+        {myUser ? <Link to="/checkout" className='btn'>
           proceed to checkout
-        </Link>
+        </Link> : <button type="button" onClick={loginWithRedirect} className="btn">login</button>}
       </div>
     </Wrapper>
   )
